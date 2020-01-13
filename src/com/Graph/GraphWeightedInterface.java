cb86394b4330d741275c0c351ba2fb5b22fabbcc
Andranik Hayrapetyan <47415182+andranik7@users.noreply.github.com>
2020-01-13 15:46:45 +0100

    Updated graph interface

--- src/com/Graph/GraphWeightedInterface.java
+++ src/com/Graph/GraphWeightedInterface.java
@@ -1,10 +1,18 @@
 package com.Graph;
 
 import java.util.HashSet;
+import java.util.List;
+import java.util.ArrayList;
+
 
 interface GraphWeightedInterface {
     void addEgde(int source, int destination, int weight);
     void printGraph();
+    Vertex getVertexById(String id);
+    List<Vertex> getVertexNeighbors(Vertex vertex);
+    Edge getEdge(Vertex src, Vertex dest);
+    
+    
 
 
 }
